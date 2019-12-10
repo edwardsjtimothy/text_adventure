bc37600a1c83bd9d5fd65aa951906a7c946468aa
Enragedtoast <edwardsjtimothy@gmail.com>
2019-12-09 18:18:05 -0600

    working on character creation constructors

--- adventurers.py
+++ adventurers.py
@@ -4,5 +4,19 @@ import random
 #player classes
 
 class Wizard: 
-    health = 10 + random.randrange(1,10)
-    mana = 20 + random.randrange(10,30)
+    name = ""
+    health = 0
+    mana = 0
+
+    def __init__(self, me, hp, mn):
+        self.name = me 
+        self.health = hp + random.randrange(5, 15)
+        self.mana = mn + random.randrange(20, 40)
+
+    # def display(self): 
+    #     print("Health = " + str(self.health) + random.randrange(5, 15)) 
+    #     print("Mana = " + str(self.mana) + random.randrange(20, 40)) 
+
+    
+       
+    
--- genesis.py
+++ genesis.py
@@ -1,8 +1,17 @@
 #importing creature constructor function
-from adventurers import *
+from adventurers import Wizard
+
+#character name function 
+
+def name():
+    print("************What is your name, traveller?***********")
+    global name
+    name = input()
+    print("Hello, " + name)
 
 #main menu function
 def main():
+    name()
     print("**********Choose your Class**********")
     choice = input("""
         A: Wizard
@@ -11,10 +20,20 @@ def main():
               """)
     if choice == "A" or choice =="a":
         print("You have selected Wizard!")
+        char = Wizard(name, 10, 20)
+        stats = """
+            ||Name: {} ||
+            ||Health: {} ||
+            ||Mana: {} ||
+        """
+        print(stats.format(char.name, char.health, char.mana))
     elif choice == "B" or choice =="b":
         print("You have selected Rogue!")
     elif choice == "C" or choice =="c":             
         print("You have selected Barbarian!")
+
+
+
 
 
 main()
