36f09659dc83ba2adab43766e39775ee8889fb14
Enragedtoast <edwardsjtimothy@gmail.com>
2019-12-14 12:28:53 -0600

    trimmed exposition text

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 from events.genesis import *
 from events.combat import *
 from creatures.monsters import *
+from events.narrative.chapter1 import *
 
+
+exposition()
 main()
 wizEncounter("An enemy approaches...", targetDummy)
