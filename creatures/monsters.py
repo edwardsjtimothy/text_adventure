0c38493383511dc4523125e8241ea6dab3ac8191
Enragedtoast <edwardsjtimothy@gmail.com>
2020-01-02 18:35:53 -0600

    refactoring

--- creatures/monsters.py
+++ creatures/monsters.py
@@ -112,7 +112,6 @@ class Skeleton:
         elif whichAbility == 6:
             self.collapse()
 
-
 ancientSkeleton = Skeleton("Ancient Skeleton", 5, 10)
 crumblingSkeleton = Skeleton("Crumbling Skeleton", 3, 7)
 giantSkeleton = Skeleton("Giant Skeleton", 10, 12)
