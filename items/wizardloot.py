de9577979a628e51cc844f243e65743dbac5cf09
Enragedtoast <edwardsjtimothy@gmail.com>
2020-01-08 11:31:57 -0600

    defining wizard item name dictionaries

--- items/wizardloot.py
+++ items/wizardloot.py
@@ -2,8 +2,24 @@
 import random 
 
 class Wizardloot:
+    consumableNames = {
+        first: ["Draft of", "Potion of", "Salve of", "Tincture of"],
+        second: ["Stupendous", "Mystical", "Miraculous", "Occult", "Esoteric", "Lightning", "Divine"],
+        third: ["Healing", "Strength", "Fortitude", "Magical Force"]
+    }
+
+    weaponNames = {
+        first: ["The", "Garblov's", "Paracelsus'"],
+        second: ["Staff", "Dagger", "Blade", "Anthame", "Wand", "Shillelagh"],
+        third: ["of Unbridled Power", "of Insidious Force", ", Wizard's Bane", "Death", "of Terrible Laughter", "Creeping Darkness", "Divine Radiance", "Thunderous Retort", "of the Unknown Frontier", "of Adamantine Will"]
+    }
     consumable: False
     weapon: False
     armor: False
 
     def __init__(self, con, wep, arm):
+        self.consumable = con
+        self.weapon = wep
+        self.armor = arm
+
+        if self.
